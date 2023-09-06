cfd2d8d86faaf858d602c6973d6cd19506b0366c
DonovanAlexander123 <144191156+DonovanAlexander123@users.noreply.github.com>
2023-09-05 19:52:50 -0600

    Add files via upload

---
+++ p2_1S21.java
@@ -0,0 +1,12 @@
+ class p2_1S21 {
+ 	public static void main(String[] args) {
+ 		System.out.println("Datos personales:");
+ 		System.out.println("Donovan Axl Jimenez Vargas");
+ 		System.out.println("18 años");
+ 		System.out.println("donovanjva0@gmail.com");
+ 		System.out.println("Jugar videojuegos y dormir");
+ 		System.out.println("Mi color favorito es el negro");
+
+ 	}
+ 	}
+ 
